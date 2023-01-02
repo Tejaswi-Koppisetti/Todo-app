781cb819debc61a50be141ba8942ced3e64933ef
Tejaswi-Koppisetti <112086071+Tejaswi-Koppisetti@users.noreply.github.com>
2023-01-03 00:15:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+
 const port = process.env.PORT || 3000;
-app.listen(3000, () => {
-  console.log(`server listening at port - ${port}`);
+app.listen(port, () => {
+  console.log(`Started express server at ${port}`);
 });
