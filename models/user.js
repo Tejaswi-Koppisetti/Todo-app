f3f859fb7cc979427251228f73157d8ad63ad8b0
Tejaswi-Koppisetti <112086071+Tejaswi-Koppisetti@users.noreply.github.com>
2023-01-08 22:40:49 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate(models) {
       // define association here
       User.hasMany(models.Todo, {
